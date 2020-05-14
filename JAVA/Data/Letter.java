47c3724fdade4ab18e6ae52a55010b079932e1fd
act-think <angel4yj@daum.net>
2020-05-15 02:34:37 +0900

    a

---
+++ JAVA/Data/Letter.java
@@ -0,0 +1,15 @@
+public class Letter {
+ 
+    public static void main(String[] args) {
+    	
+    	
+        String name = "leezche";
+        System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+        
+        System.out.println(10);
+        
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+ 
+}
---
+++ JAVA/Data/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+ 
+    public static void main(String[] args) {
+        
+    	a=1; // error
+    	
+        int a = 1; // Number -> integer  ... -2, -1 , 0, 1, 2 ...
+        
+        int b = 1.1; //error
+         
+        double b = 1.1; // real number -> double ... -1.5, -1.0, 0, 1.0, 1.5 ...
+         
+        String c = "Hello World";
+
+    }
+ 
+}
