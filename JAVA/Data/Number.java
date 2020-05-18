e2506084f53556e0fed7a3da14e7c423dafb5a3a
act-think <angel4yj@daum.net>
2020-05-18 13:02:01 +0900

    a

--- JAVA/Data/Number.java
+++
@@ -1,9 +0,0 @@
-public class Number{
-	public static void main(String[] args) {
-		
-		 System.out.println("Hello World".length()); // 11
-	     System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
-	 
-
-	}
-}
